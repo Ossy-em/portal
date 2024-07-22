3e1f9ba6fd1f3383676350b676fa13646d97e551
Ossy-em <emosinachi@gmail.com>
2024-07-22 11:18:38 +0100

    Push

--- ictrequestportal/src/components/CompanyName/CompanyName.jsx
+++ ictrequestportal/src/components/CompanyName/CompanyName.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './CompanyName.css'
+import images from '../CompanyName/NSCLogo.png'
+
 const CompanyName = () => {
   return (
     <div className='CompanyName'>
-      <h3>Nigeria Shippers Council</h3>
+   <div className='images'>  
+    
+     <img src={images} alt='image' style={{ width: '180px', height: '150px' }}/>
+   
+   </div>
+      {/* <h3>Nigeria Shippers Council</h3> */}
       
     </div>
   )
--- ictrequestportal/src/components/Form/Form.jsx
+++ ictrequestportal/src/components/Form/Form.jsx
@@ -10,17 +10,17 @@ const Form = () => {
       </div>
 
       <div className="form-group">
-        <label htmlFor="lastName">Last Name</label>
+        <label htmlFor="lastName">Last Name</label> 
         <input type="text" id="lastName" name="lastName" required />
       </div>
       
       <div className="form-group">
-        <label htmlFor="department">Department</label>
+        <label htmlFor="department">Division/Unit/Department</label>
         <input type="text" id="department" name="department" required />
       </div>
       
       <div className="form-group">
-        <label htmlFor="manager">Manager</label>
+        <label htmlFor="manager">Director Head</label>
         <input type="text" id="manager" name="manager" required />
       </div>
       
@@ -28,11 +28,17 @@ const Form = () => {
         <label htmlFor="employeeFloor">Employee Floor</label>
         <input type="text" id="employeeFloor" name="employeeFloor" required />
       </div>
+
+      <div className="form-group">
+        <label htmlFor="desiredDate">Purpose</label>
+        <input type="text" id="desiredDate" name="desiredDate" required />
+      </div>
       
       <div className="form-group">
         <label htmlFor="desiredDate">Desired Date</label>
         <input type="date" id="desiredDate" name="desiredDate" required />
       </div>
+
       
       <button type="submit">Submit Request</button>
 
